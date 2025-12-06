50e39081bbd38803e5e15b9d21783840a82d8ba6
dan <151111535+shwdaniel7@users.noreply.github.com>
2025-12-05 22:38:02 -0300

    Update Supabase anon key in configuration

--- js/config/supabaseClient.js
+++ js/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = 'https://yijgjxbznkanpsytcpjl.supabase.co'; // Cole sua URL aqui
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpamdqeGJ6bmthbnBzeXRjcGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwODM1OTIsImV4cCI6MjA3NDY1OTU5Mn0.2H2nOwA3H45Cg9rwtbZ32MgMaMuKIH2P8Qqol--rbJg';
+const SUPABASE_ANON_KEY = 'sb_publishable_uP1ivxD4Xf-J1xRxK8qCwA_PJRVtxBX';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
